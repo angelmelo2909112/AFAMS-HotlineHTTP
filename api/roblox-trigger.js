6d8fee36db75461b51410840c49c16c87e7ebd75
angelmelo2909112 <angelmelo290911@gmail.com>
2026-02-05 19:51:25 +0100

    Update roblox-trigger.js

--- api/roblox-trigger.js
+++ api/roblox-trigger.js
@@ -4,13 +4,15 @@ export default function handler(req, res) {
   if (req.method !== 'POST') return res.status(405).send("Method not allowed")
 
   let code
+
   if (req.headers['content-type']?.includes("application/json")) {
-    code = req.body.server_code
+    code = req.body?.server_code
   } else {
-    code = req.body.server_code || parse(req.body || "").server_code
+    code = req.body?.server_code || parse(req.body || "").server_code
   }
 
   const activeServers = global.activeServers || {}
+
   if (!code || !activeServers[code]) {
     return res.status(400).send("Invalid code")
   }
