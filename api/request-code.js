a4da015136b2b5fa20b420e35e69ce772f37d68e
angelmelo2909112 <angelmelo290911@gmail.com>
2026-02-05 15:33:27 +0100

    Update request-code.js

--- api/request-code.js
+++ api/request-code.js
@@ -1,16 +1,18 @@
-let activeServers = global.activeServers || {}
-global.activeServers = activeServers
+global.codeOwners ||= {}
+global.activeTriggers ||= {}
 
 export default function handler(req, res) {
   if (req.method !== 'POST') return res.status(405).send("Method not allowed")
+
   const { serverId } = req.body
   if (!serverId) return res.status(400).send("serverId required")
 
   let code
   do {
     code = Math.floor(10 + Math.random() * 990).toString().padStart(3,'0')
-  } while (activeServers[code])
+  } while (global.codeOwners[code])
 
-  activeServers[code] = serverId
+  global.codeOwners[code] = serverId
+
   res.status(200).json({ code })
 }
